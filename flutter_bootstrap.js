114e18e257b01dee7cb3933d3859c1a94b03eebb
Usamasoft <usamaaqeel.softecint@gmail.com>
2025-09-18 12:40:29 +0500

    build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3062758318"
+    serviceWorkerVersion: "254374261"
   }
 });
